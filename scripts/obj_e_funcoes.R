9a76bdf439907bda18bf8d94e8f46797b2d2a0b9
Mauro Azevedo de Siqueira Filho <desenvolvedormauro@gmail.com>
2022-09-07 17:21:01 -0300

    Atividade objetos e funções - atualizada

--- scripts/obj_e_funcoes.R
+++ scripts/obj_e_funcoes.R
@@ -4,4 +4,4 @@ vetor <- c(2,4,6,8) # criação de um vetor
 
 regCarros <- lm(mpg ~ ., mtcars) #criação de um modelo de regressão
 
-str(regCarros)
+str(regCarros) # apresentação da complexidade do objeto
