0abe0316a71b9cdabce0a32208d3f9e8ff6ca825
Mauro Azevedo de Siqueira Filho <desenvolvedormauro@gmail.com>
2022-09-07 18:28:36 -0300

    Aula do professor hugo trabalhando com pacotes

---
+++ scripts/aulaHugoPacotes.R
@@ -0,0 +1,18 @@
+# Aula do professor hugo trabalhando com pacotes
+
+#trabalhando com pacotes
+
+# instala o pacote caret (machine learning)
+install.packages( " acento circunflexo " )
+
+#carregue o pacote acento circunflexo
+biblioteca ( caret )
+featurePlot( x  =  iris [, 1 : 4 ], y  =  iris $ Species )
+
+# carga
+caret :: featurePlot( x  =  iris [, 1 : 4 ], y  =  iris $ Species ) # plot das variáveis ​​do conjunto de dados iris, usando o pacote caret
+
+#pacman _
+install.packages( ' pacman ' )
+
+pacman :: p_load( caret , VIM ) # carrega se o pacote estiver instalado. Se não estiver, instalado.
