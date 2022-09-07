143b1beb02300524a58f658c087679ae64107c4d
Mauro Azevedo de Siqueira Filho <desenvolvedormauro@gmail.com>
2022-09-07 17:48:11 -0300

    Exercício de centralização de uma base de dados. Estatistica descritiva.

---
+++ scripts/estatistica_descri.R
@@ -0,0 +1,31 @@
+Centralizando uma base de dados
+
+#  Centralização de uma base de dados
+
+binomialnegSimulacao <- rnbinom(450, mu = 3, size = 10)
+
+poissonSimulacao <- rpois(450, 3)
+
+hist(binomialnegSimulacao)
+
+hist(poissonSimulacao)
+
+binomialnegSimulacao + poissonSimulacao # soma das distribuições
+
+poissonSimulacao + 50 # soma 50 a cada elemento
+
+# média
+mean(poissonSimulacao)
+mean(binomialnegSimulacao)
+
+# mediana
+median(poissonSimulacao)
+median(binomialnegSimulacao)
+
+# desvio padrão 
+sd(poissonSimulacao)
+sd(binomialnegSimulacao)
+
+# variância
+var(poissonSimulacao)
+var(binomialnegSimulacao)
